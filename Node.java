27867529f83ecc9a39cfc5cf25483d0819536ce6
tomas-20 <tacuna20@stuy.edu>
2021-01-05 20:11:59 -0500

    made Node

---
+++ Node.java
@@ -0,0 +1,22 @@
+public class Node {
+ private String data;
+ private Node next, prev;
+ public String data() {
+   return data;
+ }
+ public Node next() {
+   return next;
+ }
+ public Node prev() {
+   return prev;
+ }
+ public void setData(String thing) {
+   data = thing;
+ }
+ public void setNext(Node thing) {
+   next = thing;
+ }
+ public void setPrev(Node thing) {
+   prev = thing;
+ }
+}
