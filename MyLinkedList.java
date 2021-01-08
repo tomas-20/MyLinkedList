3a4bc6a7f51466e780c231f47a32d4a26f668a2f
tomas-20 <tacuna20@stuy.edu>
2021-01-07 22:44:04 -0500

    added the remove function

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,75 +1,104 @@
 public class MyLinkedList {
- private int size;
- private Node start, end;
- public MyLinkedList() {
-   size = 0;
- }
- public int size() {
-   return size;
- }
- private void check(int n) {
-   if (n < 0 || n > size()) {
-     throw new IndexOutOfBoundsException("index " + n + " not in range of list of size " + size());
-   }
- }
- private void connect(Node a, Node b) {
-   b.setPrev(a);
-   a.setNext(b);
- }
- private Node getNode(int index) {
-   check(index);
-   Node output = start;
-   for (int i = 0; i < index; i ++) {
-     output = output.next();
-   }
-   return output;
- }
- public boolean add(int index, String value) {
-   check(index);
-   Node newNode = new Node(value);
-   if (size() == 0) {
-     start = newNode;
-     end = newNode;
-   }
-   else if (index == 0) {
-     connect(newNode, start);
-     start = newNode;
-   }
-   else if (index == size()) {
-     connect(end, newNode);
-     end = newNode;
-   }
-   else {
-     Node before = getNode(index - 1);
-     Node after = before.next();
-     connect(before, newNode);
-     connect(newNode, after);
-   }
-   size ++;
-   return true;
- }
- public boolean add(String value) {
-   return add(size(), value);
- }
- public String get(int index) {
-   return getNode(index).data();
- }
- public String set(int index, String value) {
-   Node replaced = getNode(index);
-   String output = replaced.data();
-   replaced.setData(value);
-   return output;
- }
- public String toString() {
-   String output = "[";
-   Node current = start;
-   Node next = current.next();
-   while (next != null) {
-     output += current.data() + ", ";
-     current = next;
-     next = next.next();
-   }
-   output += current.data() + "]";
-   return output;
- }
+  private int size;
+  private Node start, end;
+  public MyLinkedList() {
+    size = 0;
+  }
+  public int size() {
+    return size;
+  }
+  private void check(int n) {
+    if (n < 0 || n > size()) {
+      throw new IndexOutOfBoundsException("index " + n + " not in range of list of size " + size());
+    }
+  }
+  private void connect(Node a, Node b) {
+    b.setPrev(a);
+    a.setNext(b);
+  }
+  private Node getNode(int index) {
+    check(index);
+    Node output = start;
+    for (int i = 0; i < index; i ++) {
+      output = output.next();
+    }
+    return output;
+  }
+  public boolean add(int index, String value) {
+    check(index);
+    Node newNode = new Node(value);
+    if (size() == 0) {
+      start = newNode;
+      end = newNode;
+    }
+    else if (index == 0) {
+      connect(newNode, start);
+      start = newNode;
+    }
+    else if (index == size()) {
+      connect(end, newNode);
+      end = newNode;
+    }
+    else {
+      Node before = getNode(index - 1);
+      Node after = before.next();
+      connect(before, newNode);
+      connect(newNode, after);
+    }
+    size ++;
+    return true;
+  }
+  public String remove(int index) {
+    size --;
+    check(index);
+    if (size() == 0) {
+      Node removed = start;
+      start = null;
+      end = null;
+      return start.data();
+    }
+    else if (index == 0) {
+      Node removed = start;
+      start = start.next();
+      start.setPrev(null);
+      return removed.data();
+    }
+    else if (index == size()) {
+      Node removed = end;
+      end = end.prev();
+      end.setNext(null);
+      return removed.data();
+    }
+    else {
+      Node before = getNode(index - 1);
+      Node removed = before.next();
+      Node after = removed.next();
+      connect(before, after);
+      return removed.data();
+    }
+  }
+  public boolean add(String value) {
+    return add(size(), value);
+  }
+  public String get(int index) {
+    return getNode(index).data();
+  }
+  public String set(int index, String value) {
+    Node replaced = getNode(index);
+    String output = replaced.data();
+    replaced.setData(value);
+    return output;
+  }
+  public String toString() {
+    String output = "[";
+    Node current = start;
+    Node next = current.next();
+    while (next != null) {
+      output += current.data() + ", ";
+      current = next;
+      next = next.next();
+    }
+    output += current.data() + "]";
+    return output;
+  }
 }
--- Node.java
+++ Node.java
@@ -1,25 +1,25 @@
 public class Node {
- private String data;
- private Node next, prev;
- public Node(String s) {
-   data = s;
- }
- public String data() {
-   return data;
- }
- public Node next() {
-   return next;
- }
- public Node prev() {
-   return prev;
- }
- public void setData(String thing) {
-   data = thing;
- }
- public void setNext(Node thing) {
-   next = thing;
- }
- public void setPrev(Node thing) {
-   prev = thing;
- }
+  private String data;
+  private Node next, prev;
+  public Node(String s) {
+    data = s;
+  }
+  public String data() {
+    return data;
+  }
+  public Node next() {
+    return next;
+  }
+  public Node prev() {
+    return prev;
+  }
+  public void setData(String thing) {
+    data = thing;
+  }
+  public void setNext(Node thing) {
+    next = thing;
+  }
+  public void setPrev(Node thing) {
+    prev = thing;
+  }
 }
